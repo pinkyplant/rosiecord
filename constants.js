10ee3ac132abfdb50abc0a85b9d1fab59fecc57e
jay <pinkyplantz@proton.me>
2024-12-27 15:13:51 +0000

    Update constants.js
    To 261

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_223.0_57230.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_261.0_69313.ipa;
 export { Shell, Colors, Divider, States, Constants };
